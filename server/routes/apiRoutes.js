f2e6d762f7ce295ff56f2d1aa5f886cbdbc6c292
Enis Shallci <enisshallci@gmail.com>
2023-04-14 00:03:02 +0200

    new commits

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+
 const productRoutes = require("./productRoutes.js")
+const orderRoutes = require("./orderRoutes.js")
 
 router.get("/products", productRoutes)
+
+router.get("/orders", orderRoutes)
 
 module.exports = router

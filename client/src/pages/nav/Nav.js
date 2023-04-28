6740e6e6b41ab3dd15fabadc61d600b02f045431
yllkabicaj <y.bicaj@gmail.com>
2023-04-28 22:20:34 +0200

    Krijimi i Faqes About

--- client/src/pages/nav/Nav.js
+++ client/src/pages/nav/Nav.js
@@ -1,10 +1,22 @@
 import { Link } from 'react-router-dom'
 import './Nav.css'
+import {BsPersonCircle} from 'react-icons/bs'
+import {BiChevronDown} from 'react-icons/bi'
+import { useState } from 'react';
+import {GoChevronUp} from 'react-icons/go'
+import logo from "../../content/logo.png"
 
 export const Nav = ()=>{
+  const [displayy, setDisplay] = useState('none');
+  const DisplayFlexHandler =()=>{
+    setDisplay("flex");
+}
+const DisplayNoneHandler =()=>{
+    setDisplay("none");
+}
     return(
-        <div className='Nav'>
-            <div className='Logo' data-aos="fade-right">TechGalaxy</div>
+        <div className='Nav' onClick={DisplayNoneHandler}>
+            <div className='Logo' data-aos="fade-right"><img src={logo}></img></div>
             <div className='Menu' data-aos="fade-left">
             <ul>
     <li>
@@ -16,8 +28,13 @@ export const Nav = ()=>{
     <li>
       <Link to="/contact">Contact</Link>
     </li>
-    <li>
-      <Link to="/register">Register</Link>
+    <li className='dropdown' onMouseEnter={DisplayFlexHandler}>
+      <BsPersonCircle/><BiChevronDown/>
+      <ul onClick={DisplayNoneHandler} style={{display:displayy,color:"white"}}>
+      <GoChevronUp className='closeButton'/>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to={"/login"}> Login </Link></li>
+      </ul>
     </li>
   </ul>
             </div>

6902dbb6cd327066d9c95295df09a8295a489e91
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2023-05-25 19:03:25 +0200

    Create Client / React-Setup

---
+++ client/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './../components/Layout/Layout'
+
+const About = () => {
+    return (
+       <Layout title={'About Us - GalaxyTech'}>
+       <h1>About Page</h1>
+       </Layout>
+    )
+
+}
+
+export default About
---
+++ client/src/pages/Policy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './../components/Layout/Layout'
+
+const Policy = () => {
+    return (
+       <Layout title={'Policy - GalaxyTech'}>
+       <h1>Policy Page</h1>
+       </Layout>
+    )
+
+}
+
+export default Policy

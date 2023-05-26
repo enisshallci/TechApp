d4293cfb15ca63fd44484cc53501c2ff763f6e37
yllkabicaj <y.bicaj@gmail.com>
2023-05-27 00:12:32 +0200

    Krijimi i login dhe register

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,21 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-   
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/forgot-password" element={<ForgotPasssword />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
       </Routes>
     </>
   );

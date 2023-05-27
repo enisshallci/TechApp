4aab914b11afdf1d6f1f5ecf12246aa99ef6ae5c
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2023-05-27 13:59:49 +0200

    Modifikimi ne login / routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,20 @@ import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
         {/* <Route path="/forgot-password" element={<ForgotPasssword />} /> */}
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />

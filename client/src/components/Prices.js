7e3245e66bc28d943471d6e48ab6e7d7632d4d2a
yllkabicaj <y.bicaj@gmail.com>
2023-05-29 12:34:40 +0200

    Krijimi i category filter dhe price filter

---
+++ client/src/components/Prices.js
@@ -0,0 +1,33 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "$0 to 99",
+      array: [0, 99],
+    },
+    {
+      _id: 1,
+      name: "$100 to 299",
+      array: [100, 299],
+    },
+    {
+      _id: 2,
+      name: "$300 to 499",
+      array: [300, 499],
+    },
+    {
+      _id: 3,
+      name: "$500 to 699",
+      array: [500, 699],
+    },
+    {
+      _id: 4,
+      name: "$700 to 899",
+      array: [700, 899],
+    },
+    {
+      _id: 5,
+      name: "$900 or more",
+      array: [900, 9999],
+    },
+  ];
+  

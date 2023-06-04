93b3062180cf617581c590b1e66eedb558b1d17c
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2023-06-04 21:13:20 +0200

    Delete User

--- controllers/authController.js
+++ controllers/authController.js
@@ -252,3 +252,21 @@ export const orderStatusController = async (req, res) => {
   }
 };
 
+// Delete user
+export const deleteUserController = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    // Find the user by ID and delete it
+    const deletedUser = await userModel.findByIdAndRemove(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
